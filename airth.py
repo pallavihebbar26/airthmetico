4ca23f5f284f132cab86088f838d420560d2db5c
Pallavi.Hebbar <01fe24ica003@kletech.ac.in>
2025-11-26 18:55:07 +0530

    my first commit with addition and subtraction operations

---
+++ airth.py
@@ -0,0 +1,6 @@
+n1 = float(input("Enter the first number="))
+n2 = float(input("Enter the second number= "))
+add = n1 + n2
+sub = n1 - n2
+print(f"Addition of {n1} and {n2} is :{add}")
+print(f"Subtraction of{n2} and {n2} is :{sub}")
